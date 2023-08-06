b70306eeb18b3454483e7b3c2d84eec955d79c2b
AlexParreira <alexander.o.parreira@gmail.com>
2023-08-06 12:00:46 -0300

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Alexander Parreira on 06/08/23.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
